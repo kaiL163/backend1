6cab3e2493fd86cbebdc282d574d7d5b4599e021
qrine <kayzerov444@gmail.com>
2026-03-02 21:32:59 +0400

    Backend: Add nekostream.ru to CORS whitelist

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3001",
         "https://vh19690.ffox.site",
+        "https://nekostream.ru",
     ],
     allow_credentials=True,
     allow_methods=["*"],
